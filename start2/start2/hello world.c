8824efc42e8e917e7c533d4f16b31238ae5716d9
Cookiepie-1 <129765062+Cookiepie-1@users.noreply.github.com>
2023-04-03 22:57:33 +0800

    Add files via upload

---
+++ start2/start2/hello world.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	
+	/*printf("short=%d\n", sizeof(short));
+	printf("int=%d\n", sizeof(int));
+	printf("long=%d\n", sizeof(long));
+	printf("long long=%d\n", sizeof(long long));
+	printf("char=%d\n", sizeof(char));
+	printf("double=%d\n", sizeof(double));
+	printf("float=%d\n", sizeof(float));*/
+	int a=1;
+	//int sum=0;
+	scanf("%d", &a);
+	
+	//sum = a + b;
+	printf("%d\n",a);
+	return 0;
+}
